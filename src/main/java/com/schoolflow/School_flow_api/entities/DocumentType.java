2365fed96aa3ef681e7b38a872eeeac865575eab
LeonardoGomezz <leonardogomez306@gmail.com>
2025-09-28 11:51:09 -0500

    fix: add colum name in documenttype entity

--- src/main/java/com/schoolflow/School_flow_api/entities/DocumentType.java
+++ src/main/java/com/schoolflow/School_flow_api/entities/DocumentType.java
@@ -1,9 +1,6 @@
 package com.schoolflow.School_flow_api.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +12,7 @@ import lombok.Setter;
 public class DocumentType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
 
     private String code;
